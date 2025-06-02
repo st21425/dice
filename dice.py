079e81872415fc0d312e15fea6c0c309296a995c
st21425 <st21425@howick.school.nz>
2025-06-03 09:37:35 +1200

    Update dice.py
    added green behind background and roll counter

--- dice.py
+++ dice.py
@@ -6,7 +6,7 @@ class Roll():
         self.master = master
         self.master.title("Dice Roller")
         self.master.resizable(0,0)
-
+        self.runs = 0
         score = self.roll()
 
         self.master.rowconfigure([0,1], minsize=50)
@@ -17,27 +17,30 @@ class Roll():
 
         self.button2 = Button(self.master, text="Reroll", command=self.roll)
         self.button2.grid(row=0, column=1)
-
-        
-
-
     def quit(self):
         self.master.destroy()
     def roll(self):
+        self.runs += 1
+
         self.dice1 = random.randint(1,6)
         self.dice2 = random.randint(1,6)
-        self.score = self.dice1 + self.dice2
+        if self.dice1 == 6 and self.dice2 == 6:
+            self.label1 = Label(self.master, text =self.dice1, bg = "green")
+            self.label1.grid(row=1,column=0)
 
-        self.label1 = Label(self.master, text =self.dice1)
-        self.label1.grid(row=1,column=0)
+            self.label2 = Label(self.master, text =self.dice2, bg = "green")
+            self.label2.grid(row=1,column=1)
+        else:
+            self.label1 = Label(self.master, text =self.dice1)
+            self.label1.grid(row=1,column=0)
 
-        self.label2 = Label(self.master, text =self.dice2)
-        self.label2.grid(row=1,column=1)
+            self.label2 = Label(self.master, text =self.dice2)
+            self.label2.grid(row=1,column=1)
 
-        self.label3 = Label(self.master, text = "Total:")
+        self.label3 = Label(self.master, text = "Rolls:")
         self.label3.grid(row=2,column=0)
 
-        self.label4 = Label(self.master, text = self.score)
+        self.label4 = Label(self.master, text = self.runs)
         self.label4.grid(row=2,column=1)
 
 
